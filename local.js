51874bf8284e003b247ecc1fee3778896ef0a26e
unknown <>
2017-03-25 23:14:37 +0800

    https 链接

--- local.js
+++ local.js
@@ -1,33 +1,47 @@
-var http = require('http');
-var url = require('url');
-var crypto = require('crypto');
-var zlib = require('zlib');
+const http = require('http');
+const url = require('url');
+const crypto = require('crypto');
+const zlib = require('zlib');
+const serializeError = require('serialize-error');
 
-var server = http.createServer();
+const server = http.createServer();
 
-var keepAliveAgent = new http.Agent({
+const keepAliveAgent = new http.Agent({
     keepAlive: true,
     keepAliveMsecs: 50 * 1000,
 });
 
-var lib = require('./lib');
+const lib = require('./lib');
+
+// https://github.com/jshttp/cookie/blob/master/index.js#L28
+const fieldContentRegExp = /^[\u0009\u0020-\u007e\u0080-\u00ff]+$/;
+const fieldContentRegExpReplace = /[^\u0009\u0020-\u007e\u0080-\u00ff]/g;
+
+const CONF = require('./config.json');
 
 server.on('request', function (req, res) {
 
-    var parsed = url.parse(req.url);
+    const parsed = url.parse(req.url);
+    const headers = req.headers;
+    // remove invalid cookie
+    if (headers.cookie) {
+        if (fieldContentRegExp.test(headers.cookie) === false) {
+            headers.cookie = headers.cookie.replace(fieldContentRegExpReplace, '');
+        }
+    }
 
     const reqConfig = {
         host: parsed.host,
         port: parsed.port || 80,
         path: parsed.path,
         method: req.method,
-        headers: req.headers
+        headers: headers
     };
 
     lib.compressReqCfg(reqConfig, function (err, reqCfgBase64) {
 
-        var proxyReq = http.request({
-            hostname: 'localhost',
+        const proxyReq = http.request({
+            hostname: CONF.hostname,
             port: 8002,
             path: '/proxyhttp',
             headers: {
@@ -38,16 +52,133 @@ server.on('request', function (req, res) {
         });
 
         proxyReq.on('response', function (remmoteRes) {
-            console.log(remmoteRes.headers);
             res.writeHead(remmoteRes.statusCode, remmoteRes.headers);
             remmoteRes.pipe(res);
-
         });
 
         req.pipe(proxyReq);
+
+        proxyReq.on('error', function (err) {
+            let errstr = JSON.stringify(serializeError(err), null, 4);
+            if ('ENOTFOUND' == err.code) {
+                res.writeHead(404);
+            } else {
+                res.writeHead(500);
+            }
+            res.end(errstr);
+        });
+
     });
-
-
 });
 
-server.listen(8001);
+const uuid = require('uuid');
+const _ = require('lodash');
+
+
+server.on('connect', function (req, socket, head) {
+
+    socket.pause();
+
+    const parts = req.url.split(':');
+    const host = parts[0];
+    const port = parseInt(parts[1]) || 443;
+    const opts = { host: host, port: port };
+    const uid = uuid.v1();
+
+    const connectReq = http.request({
+        hostname: CONF.hostname,
+        port: CONF.remote_port,
+        path: '/httpsconnect',
+        headers: {
+            uid: uid,
+            conncfg: Buffer.from(JSON.stringify(opts)).toString('base64')
+        },
+        method: 'GET',
+        agent: keepAliveAgent
+    });
+
+    connectReq.on('response', function (remmoteRes) {
+        _synReply({
+            socket,
+            code: 200,
+            reason: 'Connection established',
+            headers: {
+                'Connection': 'keep-alive'
+            },
+        });
+
+        let bufQueue = [];
+        let sendReq = _.debounce(function () {
+            const queueToSend = Buffer.concat(bufQueue);
+            bufQueue = [];
+
+            doHttpUp({
+                uid,
+                buf: queueToSend
+            });
+        }, 300, { 'maxWait': 400 });
+
+        socket.on('data', function (buf) {
+            bufQueue.push(buf);
+            sendReq();
+        });
+
+        remmoteRes.on('data', function (data) {
+            socket.write(data);
+        });
+
+        remmoteRes.on('end', function () {
+            socket.destroy();
+        });
+
+        socket.resume();
+    });
+
+    connectReq.on('error', function () {
+        _synReply({
+            socket,
+            code: 502,
+            reason: 'connect remote error',
+            headers: {}
+        });
+    });
+
+    connectReq.end();
+});
+
+
+function _synReply({ socket, code, reason, headers }) {
+    const statusLine = 'HTTP/1.1 ' + code + ' ' + reason + '\r\n';
+    let headerLines = '';
+    for (let key in headers) {
+        headerLines += key + ': ' + headers[key] + '\r\n';
+    }
+    socket.write(statusLine + headerLines + '\r\n', 'UTF-8');
+}
+
+function doHttpUp(cfg) {
+    const { uid, buf } = cfg;
+    const dataReq = http.request({
+        hostname: CONF.hostname,
+        port: CONF.remote_port,
+        path: '/httpsup',
+        headers: {
+            uid: uid,
+            'Content-Length': Buffer.byteLength(buf)
+        },
+        method: 'GET',
+        agent: keepAliveAgent
+    });
+
+    dataReq.on('error', function (e) {
+        console.log(e)
+    });
+
+    dataReq.write(buf);
+    dataReq.end();
+}
+
+
+server.listen(CONF.local_port, function() {
+    console.log(`listening on ${CONF.local_port}`);
+});
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-var http = require('http');
-var url = require('url');
-var zlib = require('zlib');
+const http = require('http');
+const url = require('url');
 
-var lib = require('./lib');
+const lib = require('./lib');
+const EventEmitter = require('events');
+const net = require('net');
+const serializeError = require('serialize-error');
 
-var keepAliveAgent = new http.Agent({
+const ee = new EventEmitter();
+
+const keepAliveAgent = new http.Agent({
     keepAlive: true,
     keepAliveMsecs: 50 * 1000,
 });
 
-var server = http.createServer(function (req, res) {
+const CONF = require('./config.json');
+
+const server = http.createServer(function (req, res) {
 
     if (req.url === '/proxyhttp') {
 
@@ -24,22 +30,93 @@ var server = http.createServer(function (req, res) {
         }
 
         lib.decompressCfg(reqcfgRaw, function (err, reqcfg) {
-
-            console.log(reqcfg);
-
-            //reqcfg.agent = keepAliveAgent;
-
+            reqcfg.agent = keepAliveAgent;
             var proxyReq = http.request(reqcfg);
 
             proxyReq.on('response', function (remoteRes) {
-
-                console.log(remoteRes.headers);
-
                 res.writeHead(remoteRes.statusCode, remoteRes.headers);
                 remoteRes.pipe(res);
             });
 
             req.pipe(proxyReq);
+
+            proxyReq.on('error', function (err) {
+                let errstr = JSON.stringify(serializeError(err), null, 4);
+                if ('ENOTFOUND' == err.code) {
+                    res.writeHead(404);
+                } else {
+                    res.writeHead(500);
+                }
+                res.end(errstr);
+            });
+        });
+
+    } else if (req.url === '/httpsconnect') {
+
+        let conncfgRaw = Buffer.from(req.headers.conncfg, 'base64');
+        let conncfg = JSON.parse(conncfgRaw);
+        let uid = req.headers.uid;
+
+        if (!conncfg) {
+            res.statusCode = 500;
+            res.end('should provide reqcfg');
+            return;
+        }
+
+        let target = net.connect(conncfg);
+        let connected = false;
+        let err = null;
+
+        target.on('connect', function () {
+            connected = true;
+
+            res.writeHead(200, {});
+            res._send('');
+        });
+
+        target.on('data', function (data) {
+            res.write(data);
+        });
+
+        ee.on(uid, function (data) {
+            target.write(data);
+        });
+
+        target.on('close', function () {
+            if (err) {
+                let errstr = JSON.stringify(serializeError(err), null, 4);
+                res.end(errstr);
+            } else {
+                res.end();
+            }
+            ee.removeAllListeners(uid);
+        });
+
+        target.on('error', function (_err) {
+            err = _err;
+
+            if (!connected) {
+                res.writeHead(500);
+            }
+        });
+
+    } else if (req.url === '/httpsup') {
+
+        let uid = req.headers.uid;
+
+        if (!uid) {
+            res.statusCode = 500;
+            res.end('should provide uid');
+            return;
+        }
+
+        req.on('data', function (data) {
+            ee.emit(uid, data);
+        });
+
+        req.on('end', function () {
+            res.statusCode = 200;
+            res.end();
         });
 
     } else {
@@ -48,4 +125,6 @@ var server = http.createServer(function (req, res) {
 });
 
 
-server.listen(8002);
+server.listen(CONF.remote_port, function() {
+    console.log(`listening on ${CONF.remote_port}`);
+});
