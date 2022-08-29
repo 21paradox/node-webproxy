51d1b25c0f7f2889421d94bdcd7e17689ae787da
shiwenzhe <shiwenzhe@lixiang.com>
2022-08-29 12:57:21 +0800

    options bypass

--- local-cf.js
+++ local-cf.js
@@ -18,8 +18,24 @@ const httpAgent = new http.Agent({
 const proxy = httpProxy.createProxyServer(); // See (†)
 
 const server = http.createServer(function (req, res) {
+  if (req.method.toLowerCase() === 'options') {
+    let allowHeaders = "*";
+    const accessHeaders = req.headers['Access-Control-Request-Headers']
+    if (accessHeaders) {
+      allowHeaders = accessHeaders
+    }
+    res.setHeader('access-control-allow-origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', allowHeaders)
+    res.setHeader('access-control-allow-methods', "GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS")
+    res.setHeader('access-control-max-age', '1728000')
+    res.statusCode = 204
+    res.end('')
+    return
+  }
+
   const url1 = req.url.replace(/^\//, '')
-  req.url = '/' + encryptUrl(url1)
+  const url2 = encryptUrl(url1)
+  req.url = '/' +url2
 
   proxy.web(req, res, {
     target: conf.cfOrigin,
@@ -29,4 +45,5 @@ const server = http.createServer(function (req, res) {
   });
 });
 
-server.listen(8866);
+server.listen(conf.cfPort);
+console.log('listen ' + conf.cfPort)
