49bdcae97420eb3609bf3d1baf121b74c60d6029
shiwenzhe <shiwenzhe@lixiang.com>
2023-05-25 12:30:14 +0800

    add ipv6& ws support

--- local-cf.js
+++ local-cf.js
@@ -12,10 +12,16 @@ function encryptUrl(str) {
 const httpAgent = new http.Agent({
   keepAlive: true,
   keepAliveMsecs: 50 * 1000,
-  lookup: staticLookup(conf.cfip, 4),
+  // lookup: staticLookup(conf.cfip, 4),
+  lookup: staticLookup(conf.cfip6, 6),
 });
 
-const proxy = httpProxy.createProxyServer(); // See (†)
+const proxy = httpProxy.createProxyServer({
+  target: conf.cfOrigin1,
+  agent: httpAgent,
+  changeOrigin: true,
+  toProxy: true
+}); // See (†)
 
 const server = http.createServer(function (req, res) {
   if (req.method.toLowerCase() === 'options') {
@@ -33,17 +39,35 @@ const server = http.createServer(function (req, res) {
     return
   }
 
-  const url1 = req.url.replace(/^\//, '')
+  let url1 = req.url.replace(/^\//, '')
+  url1 = url1.replace(/(^https:)\/([^/])/, 'https://$2')
+
   const url2 = encryptUrl(url1)
   req.url = '/' +url2
 
   res.setHeader('access-control-allow-origin', '*')
   proxy.web(req, res, {
-    target: conf.cfOrigin,
+    target: conf.cfOrigin1,
     agent: httpAgent,
     changeOrigin: true,
     toProxy: true,
+  }, function (err) {
+    // Now you can get the err
+    // and handle it by your self
+    // if (err) throw err;
+    // res.writeHead(502);
+    res.end("There was an error proxying your request");
   });
+});
+
+server.on('upgrade', function (req, socket, head) {
+  let url1 = req.url.replace(/^\//, '')
+  url1 = url1.replace(/(^https:)\/([^/])/, 'https://$2')
+  req.headers['X-Forwarded-For'] = conf.cfip6
+
+  const url2 = encryptUrl(url1)
+  req.url = '/' + url2
+  proxy.ws(req, socket, head);
 });
 
 server.listen(conf.cfPort);
